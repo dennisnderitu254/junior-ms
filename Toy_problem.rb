6d9b7840143256e38860c4003025165edf8cdf54
Dennis Nderitu <dknderitu@gmail.com>
2015-07-23 14:22:46 +0300

    This is the new redefined toy problem

---
+++ Toy_problem.rb
@@ -0,0 +1,25 @@
+math = 70
+english = 60
+kiswahili = 69
+science = 90
+physics = 84
+
+ puts sum 
+
+
+if sum > 400
+    print "A"
+elsif
+    sum < 400 && sum > 380
+    print "A-"
+elsif
+     sum < 380 && sum > 350
+     print "B+"
+elsif sum <350 && sum > 320
+     print "C+"
+elsif  sum <320 && sum> 300
+    print "D+"
+else
+    print "Fail"
+end
+    
